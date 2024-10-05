f10e19e4f2e791d620d2703bd2dac69fdce983a6
An-Shine <akh1012@naver.com>
2024-10-05 17:09:45 +0900

    fix: logging ablog

--- Source/ArenaBattle/Fountain.cpp
+++ Source/ArenaBattle/Fountain.cpp
@@ -65,7 +65,10 @@ void AFountain::BeginPlay()
 	Super::BeginPlay();
 	
 	// logging
-	UE_LOG(ArenaBattle, Warning, TEXT("Actor Name : %s, ID: %d, Location x : %.3f"), *GetName(), ID, GetActorLocation().X);
+	//UE_LOG(ArenaBattle, Warning, TEXT("Actor Name : %s, ID: %d, Location x : %.3f"), *GetName(), ID, GetActorLocation().X);
+
+	ABLOG_S(Warning);
+	ABLOG(Warning, TEXT("Actor Name : %s, ID: %d, Location x : %.3f"), *GetName(), ID, GetActorLocation().X)
 }
 
 // Called every frame
