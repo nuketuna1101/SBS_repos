64726328e916894a2ca5f751ebd1bd1b8b3dfcc7
An-Shine <akh1012@naver.com>
2024-10-05 15:35:21 +0900

    fix: c++ fountain complete

--- Source/ArenaBattle/Fountain.cpp
+++ Source/ArenaBattle/Fountain.cpp
@@ -25,6 +25,24 @@ AFountain::AFountain()
 	Water->SetRelativeLocation(FVector(0.0f, 0.0f, 135.0f)); //높이를 1.35m 높여준다(물)
 	Light->SetRelativeLocation(FVector(0.0f, 0.0f, 195.0f)); //빛을 1.95m
 	Splash->SetRelativeLocation(FVector(0.0f, 0.0f, 195.0f)); //튀기는 모션을 1.95m
+
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> SM_BODY(TEXT("/Game/InfinityBladeGrassLands/Environments/Plains/Env_Plains_Ruins/StaticMesh/SM_Plains_Castle_Fountain_01.SM_Plains_Castle_Fountain_01"));
+	if (SM_BODY.Succeeded()) 
+	{
+		Body->SetStaticMesh(SM_BODY.Object);
+	}
+
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> SM_WATER(TEXT("/Game/InfinityBladeGrassLands/Effects/FX_Meshes/Env/SM_Plains_Fountain_02.SM_Plains_Fountain_02"));
+	if (SM_WATER.Succeeded())
+	{
+		Water->SetStaticMesh(SM_WATER.Object);
+	}
+	
+	static ConstructorHelpers::FObjectFinder<UParticleSystem> PS_SPLASH(TEXT("/Game/InfinityBladeGrassLands/Effects/FX_Ambient/Water/P_Water_Fountain_Splash_01.P_Water_Fountain_Splash_01"));
+	if (PS_SPLASH.Succeeded()) 
+	{
+		Splash->SetTemplate(PS_SPLASH.Object);
+	}
 }
 
 // Called when the game starts or when spawned
